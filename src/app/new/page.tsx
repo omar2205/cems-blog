6ede83d9667b194de0b72815428df8d5dae8dcce
omar2205 <omar2205a@yahoo.com>
2023-07-24 20:09:32 +0300

    update new post: disable submit button while creating

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { FormEvent } from 'react'
+import { FormEvent, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import axios from 'axios'
 
@@ -13,9 +13,12 @@ export const revalidate = 0
 
 export default function Home() {
   const { data: session } = useSession()
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    setIsLoading(true)
+
     const fd = new FormData(e.currentTarget)
     const body = Object.fromEntries(fd)
     const { status } = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/blog`, {
@@ -27,6 +30,7 @@ export default function Home() {
     })
 
     if (status === 201) location.href = '/'
+    setIsLoading(false)
   }
 
   return (
@@ -47,7 +51,7 @@ export default function Home() {
           </div>
 
           <div className="grid w-full max-w-sm items-center gap-3">
-            <Button>Create post</Button>
+            <Button disabled={isLoading}>Create post</Button>
             <Link className='block text-center' href="/">Cancel</Link>
           </div>
         </form>
