d41a6815c4286b393ed1ad0100be9f9dc5c0d81b
omar2205 <omar2205a@yahoo.com>
2023-07-24 15:21:03 +0300

    update header to use Link

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Header() {
   return (
     <div className="bg-neutral-800 w-full h-12">
       <div className="flex py-4 h-full max-w-2xl mx-auto justify-between items-center text-white">
-        <h1><a href="/">MySuperBlog</a></h1>
+        <h1><Link href="/">MySuperBlog</Link></h1>
         <Button>Login</Button>
       </div>
     </div>
